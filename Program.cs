54b83489602bcd0e33d43bb40a585dd5be98b619
odukoyats <topeodukoya@gmail.com>
2020-10-03 14:14:09 -0700

    i am home now

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace myAppp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        
+        /* WriteLine(); */
+        // bool isPos;
+        // isPos=false;
+         bool isPos=false;
+        int dapo=45;
+        int bebo=54;
+        uint dgdg=33;
+        ulong fff=33;
+        string push="dhgdsghdf dgdsgsd sdgsdghsd dfgetyds trtdf b";
+        char aje= 'K';
+
+
+
+        if(dapo>bebo)
+        {
+            Console.WriteLine("ggggggggggggg");
+            
+        }
+
+
+    Console.WriteLine( tel=080208542437)
+    Console. readline()
+
+        if()
+         else if(fff!=dgdg)
+         {
+        Console.WriteLine("it is not good");
+
+
+         }   
+          
+else if(fff != dgdg)
+         {
+        Console.WriteLine("it is not");
+
+         }  
+         else{
+        Console.WriteLine("thaank god");
+
+         new Console.WriteLine(tel 08028542437);} 
+         Console. readline()
+ 
+
+        }
+
+
+    }
+}
